b1af33c88e1126960e3eafe73b5ef4ac7c685f63
ggallant <ggallant@ggallant-pc.dovicosbs.local>
2011-11-28 15:03:25 -0400

    Fixed a bug where the CXMLHelper.getChildNodeValue function was using getElementsByTagName which returned sub-nodes rather than just the child node of the element queried. The result was that, under special circumstances, the wrong value would be returned for the field requested.

--- JavaCommonLibrary/src/com/dovico/commonlibrary/CXMLHelper.java
+++ JavaCommonLibrary/src/com/dovico/commonlibrary/CXMLHelper.java
@@ -12,17 +12,24 @@ public class CXMLHelper {
 	
 	
 	// Helper to pull a node's value returning the Default value should the requested node not exist or is empty
+	/// <history>
+    /// <modified author="C. Gerard Gallant" date="2011-11-28" reason="While testing a recent code modification, I discovered that xeElement.getElementsByTagName returns not just the node directly under the element queried but also any matching nodes for any sub-elements! This code has been modified to no longer use xeElement.getElementsByTagName and to loop over just the child elements."/>
+    /// </history>
 	public static String getChildNodeValue(Element xeElement, String sTagName, String sDefaultValue) {
-		// Get the list of elements with the specified tag name. If one or more elements were found then...
-		NodeList xnlTags = xeElement.getElementsByTagName(sTagName);
-		if(xnlTags.getLength() > 0)	{
-			// Grab the first item casting it to an Element		
-			Element xeItem = (Element)xnlTags.item(0);
-		
-			// Grab the first child. If we have an object then return it's value 
-			Node xnFirstChild = xeItem.getFirstChild();
-			if(xnFirstChild != null) { return xnFirstChild.getNodeValue(); }
-		} // End if(xnlTags.getLength() > 0)
+		// Loop through the element's child nodes...(we were doing xeElement.getElementsByTagName originally but it was returning us even elements of sub-nodes which
+		// is not desired)
+		for (Node xnChild = xeElement.getFirstChild(); xnChild != null; xnChild = xnChild.getNextSibling()) {
+			// If the current child node is an element AND is the element we're looking for then...
+			if ((xnChild.getNodeType() == Node.ELEMENT_NODE) && (sTagName.equals(xnChild.getNodeName()))) {
+				// Grab the first child. If we have an object then return it's value 
+				Node xnFirstChild = xnChild.getFirstChild();
+				if(xnFirstChild != null) { return xnFirstChild.getNodeValue(); }
+				
+				// We found the item we were looking for so exit this loop now (in the event the element had no text child node)
+				break;
+			} // End if ((xnChild.getNodeType() == Node.ELEMENT_NODE) && (sTagName.equals(xnChild.getNodeName())))
+		} // End of the for (Node xnChild = xeElement.getFirstChild(); xnChild != null; xnChild = xnChild.getNextSibling()) loop.
+
 		
 		// Either the tag wasn't found OR there was no value provided. Return the default value to the caller instead
 		return sDefaultValue;
